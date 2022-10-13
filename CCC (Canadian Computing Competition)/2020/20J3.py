5bbb2406fea51a3dfabf7d33923956e3374a8b31
darklyspaced <srohanjd@gmail.com>
2022-10-14 06:50:47 +0800

    20j3  and j4

--- CCC (Canadian Computing Competition)/2020/20J3.py
+++ CCC (Canadian Computing Competition)/2020/20J3.py
@@ -1,25 +1,18 @@
-drops = int(input())
-coords=[]
-tcoords=[]
-sx = 101
-sy = 101
-lx = 0
-ly = 0
+no = int(input())
+pos=[]
+sx, sy, lx, ly = 1000, 1000, 0, 0
+for i in range(no):
+    pos.append(input().split(","))
+for i in pos:
+    if int(i[0])-1 < sx:
+        sx = int(i[0])-+ 1
+    if int(i[1])-1 < sy:
+        sy = int(i[1]) - 1
+    if int(i[0])+1 > lx:
+        lx = int(i[0]) + 1
+    if int(i[1])+1 > ly:
+        ly = int(i[1]) + 1
+print(str(sx) + "," + str(sy))
+print(str(lx) + "," + str(ly))
 
-for i in range(drops):
-  tcoords.append(input())
-for i in range(len(tcoords)):
-  coords.append(tcoords[i].split(','))
-for i in range(len(coords)):
-  coords[i][0] = coords[i][0]
-for i in range(len(coords)):
-  if int(coords[i][0]) < sx:
-    sx = int(coords[i][0])
-  if int(coords[i][0]) > lx:
-    lx = int(coords[i][0])
-  if int(coords[i][1]) < sy:
-    sy = int(coords[i][1])
-  if int(coords[i][1]) > ly:
-    ly = int(coords[i][1])
-print(str(str((int(sx) - 1)) +  ',' + str(int(sy) - 1)))
-print(str(str((int(lx) + 1)) +  ',' + str(int(ly) + 1)))
+  
