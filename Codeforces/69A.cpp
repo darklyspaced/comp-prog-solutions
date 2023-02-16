d4cb870aa258d56473b2ddbea7b0c6d2a085ab9b
darklyspaced <srohanjd@gmail.com>
2023-02-16 21:00:13 +0800

    CODEFRCES: generic

--- Codeforces/69A.cpp
+++ Codeforces/69A.cpp
@@ -4,33 +4,37 @@
 using namespace std;
 bool solve(vector<vector<int>> *coords);
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int iters;
-    cin >> iters;
-    vector<vector<int>> coords;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int iters;
+  cin >> iters;
+  vector<vector<int>> coords;
 
-    for(int i = 0; i < iters; i++){
-        int x, y, z;
-        cin >> x >> y >> z;
-        vector<int> temp = {x, y, z};
-        coords.push_back(temp);
-    }
-    if (solve(&coords) == true) cout << "YES"; 
-    else cout << "NO";
-    
+  for (int i = 0; i < iters; i++) {
+    int x, y, z;
+    cin >> x >> y >> z;
+    vector<int> temp = {x, y, z};
+    coords.push_back(temp);
+  }
+  if (solve(&coords) == true)
+    cout << "YES";
+  else
+    cout << "NO";
 }
 
-bool solve(vector<vector<int>> *coords){
-    int count = 0;
-    for(int i = 0; i < 3; i++){
-        int ans = 0;
-        for (vector<int> x : *coords){
-            ans += x[i];
-        }
-        if (ans == 0) count++;
+bool solve(vector<vector<int>> *coords) {
+  int count = 0;
+  for (int i = 0; i < 3; i++) {
+    int ans = 0;
+    for (vector<int> x : *coords) {
+      ans += x[i];
     }
-    if (count == 3) return true;
-    else return false;
+    if (ans == 0)
+      count++;
+  }
+  if (count == 3)
+    return true;
+  else
+    return false;
 }
--- Codeforces/template.cpp
+++ Codeforces/template.cpp
@@ -1,31 +1,28 @@
-#include <iostream>
 #include <algorithm>
-#include <string>
-#include <stdio.h>
-#include <stack>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
 #include <queue>
 #include <set>
-#include <map>
-#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string>
 #include <unordered_map>
-#include <vector>
-#include <list>
 #include <utility>
-#include <functional>
-#include <numeric>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
 
 void solve();
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  solve();
 }
 
-void solve(){
-
-}
+void solve() {}
