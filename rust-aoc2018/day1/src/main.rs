e8471c10797529c69f399f11cd359b0fbfb54fc4
darklyspaced <srohanjd@gmail.com>
2023-04-29 20:24:45 +0800

    codingquest23 && remove defuct 23J5 solution

--- rust-aoc2018/day1/src/main.rs
+++ rust-aoc2018/day1/src/main.rs
@@ -1,5 +1,6 @@
+#![allow(dead_code)]
 fn main() {
-    part_one();
+    part_two();
 }
 
 fn part_one() {
@@ -14,5 +15,19 @@ fn part_one() {
     println!("{frequency}")
 }
 
-#[allow(unused)]
-fn part_two() {}
+fn part_two() {
+    let cali = include_str!("input.txt").lines();
+    let mut total = 0;
+    let mut prev = vec![];
+    for num in cali {
+        let sign: i8 = if num.starts_with('-') { -1 } else { 1 };
+        let freq = num[1..].parse::<i32>().unwrap() * sign as i32;
+        total += freq;
+        if prev.contains(&total) {
+            println!("{total}");
+            return;
+        }
+        prev.push(total);
+    }
+    println!("{:#?}\n", prev)
+}
